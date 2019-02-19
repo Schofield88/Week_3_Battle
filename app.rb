e73ca97f6c9acfb0fd4967b2e059fbfc8c153e94
James Ault <james.ault@johnlewis.co.uk>
2019-02-19 17:10:40 +0000

    Sessions Issues

--- app.rb
+++ app.rb
@@ -2,13 +2,26 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
+  p  session[:name1] = params[:name1]
+  p  session[:name2] = params[:name2]
+  p  session[:name1]
+  p  session[:name2]
+    redirect '/play'
+  end
+
+  get '/play' do
+ p  session[:name1].inspect
+ p  session[:name2].inspect
+
+ p   @name1 = 'James'
+ p   @name2 = 'Luke'
     erb(:play)
   end
 
