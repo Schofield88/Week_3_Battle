d0de1d5fd47075d714af11563cda858a5d6814f3
Luke Phyall <lukeprospect@yahoo.com>
2019-02-19 16:27:08 +0000

    Adds feature test for player names.

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -1,8 +1,17 @@
 require './app.rb'
 
 describe 'home page visit', type: :feature do
-  it 'Has the correct text' do
-  visit '/'
-  expect(page).to have_content 'Testing infrastructure working!'
+
+  it 'submits names and sticks them on screen' do
+    visit '/'
+    fill_in 'name1', with: 'James'
+    fill_in 'name2', with: 'Luke'
+    click_button('Submit')
+    expect(page).to have_content 'James' && 'Luke'
   end
+
+
+
+
+
 end
